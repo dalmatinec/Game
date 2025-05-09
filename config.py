748b23e249c0aab315103f939979cd90d8d16622
dalmatinec <temirbekov1602@gmail.com>
2025-05-10 00:00:43 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "7989590489:AAHtvD_jxSk4IJVhuV2hPuG9bRPHIFLI-vY"
+ADMIN_ID = [6864787140, 7998346757]
+CHAT_ID = -1002572097245
+DATABASE_URL = "postgresql://neondb_owner:npg_c1mqKueZrP6M@ep-damp-waterfall-a4i6v4yy-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
