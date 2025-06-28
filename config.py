aeb965b059496b9eec1df97e42558ed63002767a
dalmatinec <temirbekov1602@gmail.com>
2025-06-28 19:34:22 +0500

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-BOT_TOKEN = "7989590489:AAGkqqSxJl_Bhsz1M9Wvq7z1OWujA2DUTS0"
-ADMIN_ID = [6864787140, 7998346757]
-CHAT_ID = -1002540317262
-DATABASE_URL = "postgresql://neondb_owner:npg_c1mqKueZrP6M@ep-damp-waterfall-a4i6v4yy-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
